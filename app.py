6fe5a1e189b1b30e82e95545785b03fa2abcc138
David Sotto Mayor <ddsmmf@gmail.com>
2023-04-03 12:34:38 -0400

    Creating an endpoint to create new stores

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from random import randint
 
 app = Flask(__name__)
 
 stores = [
     {
         "name": "David Store",
+        "id": (randint(1, 10) * 100 * randint(1, 10)),
         "items": [
             {
                 "name": "PC",
@@ -20,6 +22,15 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/stores")
+@app.get("/store")
 def get_stores():
-    return {'stores': stores}
+    return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    body = request.get_json()
+    new_store = {"name": body["name"], "items": [],
+                 "id": (randint(1, 10) * 100 * randint(1, 10))}
+    stores.append(new_store)
+    return new_store
