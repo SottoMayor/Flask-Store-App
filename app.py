0c4381f0d2eb3bf6f09cb624bbd6f6f0ddb7c455
David Sotto Mayor <ddsmmf@gmail.com>
2023-04-03 12:13:40 -0400

    Creating an endpoint to get stores

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name": "David Store",
+        "items": [
+            {
+                "name": "PC",
+                "price": "5000"
+            }
+        ]
+    }
+]
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/stores")
+def get_stores():
+    return {'stores': stores}
