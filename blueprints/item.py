00bc24c186e6ba1a0d92242dfcff017455f24874
David Sotto Mayor <ddsmmf@gmail.com>
2023-04-06 15:52:04 -0400

    Working with Blueprints

---
+++ blueprints/item.py
@@ -0,0 +1,54 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from db import items
+
+blp = Blueprint('item', __name__, description="Operations on Items")
+
+
+class Item(MethodView):
+    def get(self, id):
+        try:
+            return items[id], 200
+        except KeyError:
+            abort(404, message="Item not found")
+
+    def put(self, id):
+        item_data = request.get_json()
+
+        try:
+            item = items[id]
+            item |= item_data
+
+            return item
+        except KeyError:
+            abort(404, message='Trying to update an inexisting item!')
+
+    def delete(self, id):
+        try:
+            del items[id]
+            return {'message': 'Item deleted successfully!'}, 200
+        except KeyError:
+            abort(404, message="Item not found")
+
+
+class ItemList(MethodView):
+    def get(self, id):
+        return {"items": list(items.values())}, 200
+
+    def post(self, id):
+        item_data = request.get_json()
+
+        for item in items.values():
+            if (item["name"] == item_data["name"] and
+                    item["store_id"] == item_data["store_id"]):
+                abort(409, message="Item already exists!")
+
+        if (item_data['store_id'] not in items):
+            abort(404, message="ID Invalid!!")
+
+        new_id = uuid.uuid4().hex
+        new_item = {**item_data, 'id': new_id}
+        items[new_id] = new_item
+        return new_item, 201
---
+++ blueprints/store.py
@@ -0,0 +1,42 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from db import stores
+
+
+blp = Blueprint('stores', __name__, description="Operations on Stores.")
+
+
+@blp.route('/store/<string:id>')
+class Store(MethodView):
+    def get(self, id):
+        try:
+            return stores[id], 200
+        except KeyError:
+            abort(404, message="Store not found")
+
+    def delete(self, id):
+        try:
+            del stores[id]
+            return {'message': 'Store deleted successfully!'}, 200
+        except KeyError:
+            abort(404, message="Store not found")
+
+
+@blp.route('/store/<string:id>')
+class StoreList(MethodView):
+    def get(self):
+        return {"stores": list(stores.values())}, 200
+
+    def post(self):
+        store_data = request.get_json()
+
+        for store in stores.values():
+            if (store["name"] == store_data["name"]):
+                abort(409, message="Store already exists!")
+
+        new_id = uuid.uuid4().hex
+        new_store = {**store_data, "id": new_id}
+        stores[new_id] = new_store
+        return new_store, 201
