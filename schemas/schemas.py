2decdcf4b7c140d2ae65c3df9c570e11bab2cb1d
David Sotto Mayor <ddsmmf@gmail.com>
2023-04-10 12:32:20 -0400

    Fixing schemas

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -4,13 +4,13 @@ from marshmallow import Schema, fields
 class ItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
-    price = fields.Str(required=True)
+    price = fields.Float(required=True)
     store_id = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
-    price = fields.Str()
+    price = fields.Float()
 
 
 class StoreSchema(Schema):
